efe55b010e3c1806c19e249027f23e170dbcb5b2
timmson <timmson666@mail.ru>
2017-01-24 22:10:00 +0300

    Add test

--- test.js
+++ test.js
@@ -1,18 +1,30 @@
-/*
+const config = require('./config.js');
 const main = require('./main.js');
-const fs = require('fs');
 
-fs.readFile('rss1.xml', (err, contents) => {
-    if (!err) {
-        feed.rss(contents, url, (err, articles) => {
-            if (err) {
-                console.error(err);
-            } else {
-                articles.slice(0, 10).forEach(article => callback(article));
-            }
-        });
-    } else {
-        console.log(err);
-    }
+log.info("Service started");
+
+main.getHosts((err, message) => {
+    err ? log.error(err.stack) : postState(message);
 });
-*/
+
+
+function getNetworkState(topic) {
+    log.info("Update network state");
+    main.getHosts((err, message) => {
+        err ? log.error(err.stack) : postState(message);
+    });
+
+}
+
+function postState(message) {
+    log.info(config.to + " <- " + message);
+    const connection = AMQP.createConnection(config.mq.connection);
+
+    connection.on('error', err => log.error("Error from amqp: " + err.stack));
+
+    connection.on('ready', () => {
+        connection.exchange(config.mq.exchange, {type: 'fanout', durable: true, autoDelete: false}, exchange =>
+            exchange.publish('', message, {}, (isSend, err) => err ? log.error(err.stack) : 0)
+        );
+    });
+}

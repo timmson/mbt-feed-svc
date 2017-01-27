1337052ce18764319941e9d75acf44f250a74b9c
KROTOV Artem <artem.krotov@raiffeisen.ru>
2017-01-27 11:09:21 +0300

    Add reverse

--- service.js
+++ service.js
@@ -18,7 +18,7 @@ let topics = [
         name: 'demo',
         channel: '@tmsnDemotivators',
         url: "http://demotivators.to/feeds/recent/",
-        cronTime: '0 0 17 * * *',
+        cronTime: '0 0 12 * * *',
         limit: 10
     }
     /*{name: 'cars', channel: '@tmsnDemotivators', url: "https://auto.mail.ru/rss/"}

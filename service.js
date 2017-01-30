c64f354187f3907eb3f602641321398403288e91
KROTOV Artem <artem.krotov@raiffeisen.ru>
2017-01-30 16:48:01 +0300

    #1 Add auto news

--- service.js
+++ service.js
@@ -5,7 +5,7 @@ const newsWatcher = require('./news-watcher.js');
 const CronJob = require('cron').CronJob;
 const log = require('log4js').getLogger('service');
 
-log.info("Service started");
+log.info('Service started');
 
 new CronJob({cronTime: config.cron, onTick: netWatcher.getNetworkState, start: true});
 
@@ -17,19 +17,35 @@ let topics = [
     {
         name: 'demo',
         channel: '@tmsnDemotivators',
-        url: "http://demotivators.to/feeds/recent/",
+        url: 'http://demotivators.to/feeds/recent/',
         cronTime: '0 0 12 * * *',
+        period: 3600000,
+        limit: 10
+    },
+    {
+        name: 'cars-auto',
+        channel: '@tmsnAutoNews',
+        url: 'https://auto.mail.ru/rss/',
+        cronTime: '0 0 10-20 * * *',
+        period: 3600000,
+        limit: 10
+    },
+    {
+        name: 'cars-motor',
+        channel: '@tmsnDemotivators',
+        url: 'http://motor.ru/export/atom',
+        cronTime: '0 0 10-20 * * *',
+        period: 3600000,
         limit: 10
     }
-    /*{name: 'cars', channel: '@tmsnDemotivators', url: "https://auto.mail.ru/rss/"}
-     {name: 'gadgets', channel: '@tmsnDemotivators', url: "http://4pda.ru/feed/"}
-     {name: 'movies', channel: '@tmsnDemotivators', url: "http://kinozal.me/rss.xml"}
-     {name: 'linux', channel: '@tmsnDemotivators', url: "http://www.linux.org.ru/section-rss.jsp?section=1"}
-     {name: 'devLife', channel: '@tmsnDemotivators', url: "http://developerslife.ru/rss.xml"}*/
+    /*{name: 'gadgets', channel: '@tmsnDemotivators', url: 'http://4pda.ru/feed/'}
+     {name: 'movies', channel: '@tmsnDemotivators', url: 'http://kinozal.me/rss.xml'}
+     {name: 'linux', channel: '@tmsnDemotivators', url: 'http://www.linux.org.ru/section-rss.jsp?section=1'}
+     {name: 'devLife', channel: '@tmsnDemotivators', url: 'http://developerslife.ru/rss.xml'}*/
 ];
 
 topics.forEach(topic => {
-    log.info("Topic " + topic.name + " started at " + topic.cronTime);
+    log.info('Topic ' + topic.name + ' started at ' + topic.cronTime);
     new CronJob(
         {
             cronTime: topic.cronTime,

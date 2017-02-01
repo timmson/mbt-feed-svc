94dbb147f0eb8855110f03941beae55936252458
timmson <timmson666@mail.ru>
2017-02-02 00:04:14 +0300

    Remove nmap

--- test.js
+++ test.js
@@ -1,16 +1,6 @@
 //const config = require('./config.js');
-const main = require('./news-watcher');
+const main = require('./net-watcher');
 
 const log = require('log4js').getLogger('net-service');
 
-log.info("Test started");
-
-main.getFeed(
-    {
-        name: 'cars-auto',
-        channel: '@tmsnAutoNews',
-        url: 'http://motor.ru/export/atom',
-        cronTime: '0 0 12 * * *',
-        period: 3600000,
-        limit: 10
-    });
+main.getNetworkState();

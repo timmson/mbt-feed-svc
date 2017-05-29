69d35a46777a3d743cc95714b23e757a176aeaa5
timmson <timmson666@mail.ru>
2017-05-30 00:03:19 +0300

    #9

--- service.js
+++ service.js
@@ -1,6 +1,7 @@
 const config = require('./config.js');
 const netWatcher = require('./net-watcher.js');
 const newsWatcher = require('./news-watcher.js');
+const weatherWatcher = require('./weather-watcher');
 
 const CronJob = require('cron').CronJob;
 const log = require('log4js').getLogger('service');
@@ -8,6 +9,8 @@ const log = require('log4js').getLogger('service');
 log.info('Service started');
 
 new CronJob({cronTime: config.cron, onTick: netWatcher.getNetworkState, start: true});
+
+new CronJob({cronTime: '0 * * * * *', onTick: weatherWatcher.getWeather(), start: true});
 
 config.topics.forEach(topic => {
     log.info('Topic ' + topic.name + ' started at ' + topic.cronTime);
---
+++ weather-watcher.js
@@ -0,0 +1,33 @@
+const config = require('./config.js');
+const log = require('log4js').getLogger('weaher-service');
+const weather = require('weather-js');
+const AMQP = require('amqp');
+
+
+module.exports.getWeather = function () {
+    log.info("Update weather");
+    weather.find({search: 'Moscow, RU', degreeType: 'C'}, function (err, result) {
+        if (!err) {
+            postState(JSON.stringify(result));
+        } else {
+            log.error(err.stack)
+        }
+    })
+};
+
+function postState(text) {
+    log.info(config.to.username + " <- " + text);
+    const message = {to: config.to, text: text};
+    const connection = AMQP.createConnection(config.mq.connection);
+
+    connection.on('error', err => log.error("Error from amqp: " + err.stack));
+
+    connection.on('ready', () =>
+        connection.exchange(config.mq.exchange, {type: 'fanout', durable: true, autoDelete: false}, exchange =>
+            exchange.publish('', JSON.stringify(message), {}, (isSend, err) => {
+                err ? log.error(err.stack) : 0;
+                connection.disconnect();
+            })
+        )
+    );
+}

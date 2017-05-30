00fa9e867f4020842c0b561bc3024bb596ee0d1d
timmson <timmson666@mail.ru>
2017-05-30 14:44:14 +0300

    #11

--- weather-watcher.js
+++ weather-watcher.js
@@ -1,19 +1,28 @@
 const config = require('./config.js');
-const log = require('log4js').getLogger('weaher-service');
+const log = require('log4js').getLogger('weather-service');
 const weather = require('weather-js');
 const AMQP = require('amqp');
 
 
-module.exports.getWeather = function () {
+module.exports.getWeather = () => weather.find({
+    search: 'Moscow, Russia',
+    lang: 'RU',
+    degreeType: 'C'
+}, (err, result) => {
     log.info("Update weather");
-    weather.find({search: 'Moscow, RU', degreeType: 'C'}, function (err, result) {
-        if (!err) {
-            postState(JSON.stringify(result['current']));
-        } else {
-            log.error(err.stack)
-        }
-    })
-};
+    if (!err) {
+        let data = result[0]['forecast'].filter(row => row.date == getTomorrow())[0];
+        postState('Завтра ' + data['low'] + 'C, ' + data['skytextday']);
+    } else {
+        log.error(err.stack)
+    }
+});
+
+function getTomorrow() {
+    let d = new Date();
+    d.setDate(d.getDate() + 1);
+    return d.toISOString().split('T')[0];
+}
 
 function postState(text) {
     log.info(config.to.username + " <- " + text);

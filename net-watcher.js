4a55fe33651de01c7a2b0eab87a73c093a72dab2
timmson <timmson666@mail.ru>
2017-06-02 09:02:32 +0300

    #13

--- net-watcher.js
+++ net-watcher.js
@@ -41,7 +41,7 @@ function getHosts(callback) {
                     callback(null, hostIp + ' ☠');
                 });
 
-                networkState.hosts = onlineHosts.map(host => host.ip);
+                networkState.hosts = onlineHosts.filter(host => host.description === '?').map(host => host.ip);
 
                 saveNetworkState(networkState, (err, res) => err ? callback(err, null) : 0);
             }

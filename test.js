f2d48ab5cc12e528767708895a0d9d39393b18ba
timmson <timmson666@mail.ru>
2017-06-17 10:01:47 +0300

    #18

--- test.js
+++ test.js
@@ -1,11 +1,4 @@
-const config = require('./config.js');
-const newsWatcher = require('./news-watcher.js');
+const regard = require('./regard-watcher');
 
-newsWatcher.getFeed({
-    name: 'football',
-    channel: '168739439',
-    url: 'https://www.championat.com/rss/article/football/russiapl/',
-    cronTime: '0 0 10-20 * * *',
-    period: 3600000,
-    limit: 15
-});
+regard.getActualGPU();
+

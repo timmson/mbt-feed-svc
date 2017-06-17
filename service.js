3d53df8d9f87c9121d9cc0e652c675a79b41061b
timmson <timmson666@mail.ru>
2017-06-17 10:14:13 +0300

    #18

--- service.js
+++ service.js
@@ -2,6 +2,7 @@ const config = require('./config.js');
 const netWatcher = require('./net-watcher.js');
 const newsWatcher = require('./news-watcher.js');
 const weatherWatcher = require('./weather-watcher');
+const regard = require('./regard-watcher');
 
 const CronJob = require('cron').CronJob;
 const log = require('log4js').getLogger('service');
@@ -16,6 +17,9 @@ new CronJob({cronTime: config.cron, onTick: netWatcher.getNetworkState, start: t
  */
 log.info('Topic Weather started at 0 0 20 * 4-10 *');
 new CronJob({cronTime: '0 0 20 * 4-10 *', onTick: weatherWatcher.getWeather, start: true});
+
+log.info('Topic Regard started at 0 0 20 * 4-10 *');
+new CronJob({cronTime: '0 0 11 * * *', onTick: regard.getActualGPU, start: true});
 
 config.topics.forEach(topic => {
     log.info('Topic ' + topic.name + ' started at ' + topic.cronTime);

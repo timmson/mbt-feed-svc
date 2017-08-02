126c217754a93972f11186ee0f3059b45082e8ef
timmson <timmson666@mail.ru>
2017-08-02 21:56:29 +0300

    #22

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
-
+const newsWatcher = require('./news-watcher.js');
+newsWatcher.getFeed(
+    {
+        name: 'demo',
+        channel: '@tmsnInstaMemes',
+        url: 'http://demotivators.to/feeds/recent/',
+        cronTime: '0 25 10-20 * * *',
+        limit: 10
+    }
+);

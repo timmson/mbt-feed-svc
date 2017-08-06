69067174030c8327216c1f3dd000b70c8de61519
timmson <timmson666@mail.ru>
2017-08-06 22:42:43 +0300

    #23

--- test.js
+++ test.js
@@ -4,4 +4,4 @@ const MessageApi = require('./modules/message-api.js');
 
 const messageApi = new MessageApi(config.telegramSvc);
 
-WeatherApi.notifyAboutWeather((text) => messageApi.sendMessage({to: config.to, type: 'text', version: '2', text: text}).catch(log.error));
+WeatherApi.notifyAboutWeather((text) => messageApi.sendMessage({to: config.to, type: 'text', version: '2', text: text}).catch(console.error));

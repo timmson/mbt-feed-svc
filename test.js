592113deb1a85017cdac8dbf80a8506dc1f60281
timmson <timmson666@mail.ru>
2017-08-06 17:17:50 +0300

    #23

--- test.js
+++ test.js
@@ -1,10 +1,6 @@
-const newsWatcher = require('./modules/news-api.js');
-newsWatcher.getFeed(
-    {
-        name: 'demo',
-        channel: '168739439',
-        url: 'http://demotivators.to/feeds/recent/',
-        cronTime: '0 25 10-20 * * *',
-        limit: 10
-    }
-);
+const config = require('./config.js');
+const NetApi = require('./modules/net-api.js');
+
+const netApi = new NetApi(config);
+
+netApi.getUnknownHosts().then(console.log);

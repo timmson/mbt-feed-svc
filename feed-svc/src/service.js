545aa5f5d072dda068b69fa03ebc8f10b2456aee
timmson <timmson666@mail.ru>
2017-12-03 21:07:17 +0300

    #31

--- feed-svc/src/service.js
+++ feed-svc/src/service.js
@@ -1,40 +1,39 @@
-const config = require('./config.js');
-config.mongo.url = process.env['db'];
+const config = require("./config.js");
+config.mongo = { url : process.env["db"]};
+const log = require("log4js").getLogger();
 
-const log = require('log4js').getLogger();
+const MessageApi = require("./modules/message-api.js");
+const NetApi = require("./modules/net-api.js");
+const NewsApi = require("./modules/news-api.js");
+const WeatherApi = require("./modules/weather-api.js");
 
-const MessageApi = require('./modules/message-api.js');
-const NetApi = require('./modules/net-api.js');
-const NewsApi = require('./modules/news-api.js');
-const WeatherApi = require('./modules/weather-api.js');
-
-const CronJob = require('cron').CronJob;
+const CronJob = require("cron").CronJob;
 
 const messageApi = new MessageApi(config.telegramSvc);
 const netApi = new NetApi(config);
 const newsApi = new NewsApi(config.mongo.url);
 
-log.info('Service started');
+log.info("Service started");
 
-log.info('Topic NetworkState started at ' + config.cron);
+log.info("Topic NetworkState started at " + config.cron);
 new CronJob({
     cronTime: config.cron,
-    onTick: () => netApi.notifyAboutUnknownHosts(text => messageApi.sendMessage({to: config.to, type: 'text', version: '2', text: text}).catch(err => log.error(err))),
+    onTick: () => netApi.notifyAboutUnknownHosts(text => messageApi.sendMessage({to: config.to, type: "text", version: "2", text: text}).catch(err => log.error(err))),
     start: true
 });
 
 /**
  * Move to config
  */
-log.info('Topic Weather started at 0 0 20 * 4-10 *');
+log.info("Topic Weather started at 0 0 20 * 4-10 *");
 new CronJob({
-    cronTime: '0 0 20 * 4-10 *',
-    onTick: () => WeatherApi.notifyAboutWeather(text => messageApi.sendMessage({to: config.to, type: 'text', version: '2', text: text}).catch(err => log.error(err))),
+    cronTime: "0 0 20 * 4-10 *",
+    onTick: () => WeatherApi.notifyAboutWeather(text => messageApi.sendMessage({to: config.to, type: "text", version: "2", text: text}).catch(err => log.error(err))),
     start: true
 });
 
 config.topics.forEach(topic => {
-    log.info('Topic ' + topic.name + ' started at ' + topic.cronTime);
+    log.info("Topic " + topic.name + " started at " + topic.cronTime);
     new CronJob(
         {
             cronTime: topic.cronTime,

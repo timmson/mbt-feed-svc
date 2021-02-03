7fd198f870ed9eee7d8cec766552331fd2a81edc
timmson <timmson666@mail.ru>
2021-02-04 00:23:20 +0300

    Improve mocks #57

--- src/test/test-moex.js
+++ src/test/test-moex.js
@@ -1,28 +1,28 @@
 const Moex = require("../lib/moex");
 
 class MockMoexAPI {
-    securityMarketData(ticker, currency) {
-        return new Promise(resolve => resolve({
-                node: {
-                    last: {
-                        "USD000UTSTOM": "75.00",
-                        "VTBA": "12.00",
-                        "VTBE": "11.00"
-                    }[ticker]
-                }
-            })
-        );
-    }
+	securityMarketData(ticker) {
+		return new Promise(resolve => resolve({
+			node: {
+				last: {
+					"USD000UTSTOM": "75.00",
+					"VTBA": "12.00",
+					"VTBE": "11.00"
+				}[ticker]
+			}
+		})
+		);
+	}
 }
 
 describe("Moex", () => {
 
-    const moex = new Moex(new MockMoexAPI());
+	const moex = new Moex(new MockMoexAPI());
 
-    test("info", () => {
-        return moex.getMessage()
-            .then((result) => expect(result).toEqual("💰75.00, 🇺🇸12.00, 🇨🇳11.00"))
-            .catch((e) => expect(e).toBeUndefined());
-    });
+	test("info", () => {
+		return moex.getMessage()
+			.then((result) => expect(result).toEqual("💰75.00, 🇺🇸12.00, 🇨🇳11.00"))
+			.catch((e) => expect(e).toBeUndefined());
+	});
 
 });

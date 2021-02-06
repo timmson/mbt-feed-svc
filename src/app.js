2b57007c7114d7df14b004794f8e848baf67cde6
Krotov Artem <timmson666@mail.ru>
2021-02-06 17:08:59 +0300

    Change cron settings #58 (#59)

--- src/app.js
+++ src/app.js
@@ -11,54 +11,71 @@ const CronJob = require("cron").CronJob;
 
 const bot = new Telegraf(config.telegram.token);
 
-log.info("Topic Weather started at " + config.cron.weather);
+const cron = {
+	weather: "0 0 7,17 * * *",
+	stock: "0 0 10-22/2 * * * "
+};
+
+log.info(`Topic Weather started at ${cron.weather}`);
 new CronJob({
-    cronTime: config.cron.weather,
-    onTick: async () => {
-        try {
-            let text = await weatherApi(new Date());
-            log.info(config.to.username + " [" + config.to.id + "]" + " <- " + text);
-            await bot.telegram.sendMessage(config.to.id, text, {"parse_mode": "HTML"});
-        } catch (err) {
-            log.error(err);
-        }
-    },
-    start: true
+	cronTime: cron.weather,
+	onTick: async () => {
+		try {
+			let text = await weatherApi(new Date());
+			log.info(config.to.username + " [" + config.to.id + "]" + " <- " + text);
+			await bot.telegram.sendMessage(config.to.id, text, {"parse_mode": "HTML"});
+		} catch (err) {
+			log.error(err);
+		}
+	},
+	start: true
 });
 
-log.info("Topic Stock started at " + config.cron.stock);
+log.info(`Topic Stock started at ${cron.stock}`);
 new CronJob({
-    cronTime: config.cron.stock,
-    onTick: async () => {
-        try {
-            let text = await stockApi();
-            log.info(config.to.username + " [" + config.to.id + "]" + " <- " + text);
-            await bot.telegram.sendMessage(config.to.id, text, {"parse_mode": "HTML"});
-        } catch (e) {
-            log.error(e);
-        }
-    },
-    start: true
+	cronTime: cron.stock,
+	onTick: async () => {
+		try {
+			let text = await stockApi();
+			log.info(config.to.username + " [" + config.to.id + "]" + " <- " + text);
+			await bot.telegram.sendMessage(config.to.id, text, {"parse_mode": "HTML"});
+		} catch (e) {
+			log.error(e);
+		}
+	},
+	start: true
 });
 
 
 bot.command("start", async (ctx) => {
-    log.info(ctx.message.from.username + " [" + ctx.message.from.id + "]" + " <- /start");
-    try {
-        await ctx.reply("Ok! Now send funny picture to me");
-    } catch (err) {
-        log.error(err);
-    }
+	log.info(ctx.message.from.username + " [" + ctx.message.from.id + "]" + " <- /start");
+	try {
+		await ctx.reply("Ok! Now send funny picture to me");
+	} catch (err) {
+		log.error(err);
+	}
 });
 
 bot.command("stock", async (ctx) => {
-    log.info(ctx.message.from.username + " [" + ctx.message.from.id + "]" + " <- /stock");
-    try {
-        let text = await stockApi();
-        await ctx.reply(text);
-    } catch (err) {
-        log.error(err);
-    }
+	log.info(ctx.message.from.username + " [" + ctx.message.from.id + "]" + " <- /stock");
+	try {
+		let text = await stockApi();
+		log.info(config.to.username + " [" + config.to.id + "]" + " <- " + text);
+		await ctx.reply(text);
+	} catch (err) {
+		log.error(err);
+	}
+});
+
+bot.command("weather", async (ctx) => {
+	log.info(ctx.message.from.username + " [" + ctx.message.from.id + "]" + " <- /weather");
+	try {
+		let text = await weatherApi(new Date());
+		log.info(config.to.username + " [" + config.to.id + "]" + " <- " + text);
+		await ctx.reply(text);
+	} catch (err) {
+		log.error(err);
+	}
 });
 
 bot.startPolling();
@@ -68,11 +85,11 @@ log.info("Service has started");
 log.info("Please press [CTRL + C] to stop");
 
 process.on("SIGINT", () => {
-    log.info("Service has stopped");
-    process.exit(0);
+	log.info("Service has stopped");
+	process.exit(0);
 });
 
 process.on("SIGTERM", () => {
-    log.info("Service has stopped");
-    process.exit(0);
+	log.info("Service has stopped");
+	process.exit(0);
 });

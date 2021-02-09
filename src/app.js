3363e1934888c1250c0b96a41af60f43e2fb7ef1
timmson <timmson666@mail.ru>
2021-02-09 11:18:48 +0300

    Change cron settings #58

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const prodCalendar = new ProdCalendar("ru");
 
 const cron = {
 	weather: "0 0 7,17 * * *",
-	stock: "0 0 10-22/2 * * * "
+	stock: "0 50 9-21/2 * * * "
 };
 
 log.info(`Topic Weather started at ${cron.weather}`);

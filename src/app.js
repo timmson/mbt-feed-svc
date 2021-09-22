3823a9c9111f29f1708f734479eb976f804f877f
timmson <timmson666@mail.ru>
2021-09-22 22:35:45 +0300

    Send message to channel #65

--- src/app.js
+++ src/app.js
@@ -14,8 +14,7 @@ const prodCalendar = new ProdCalendar("ru");
 
 const cron = {
 	weather: "0 0 7,17 * * *",
-	/*stock: "0 50 9-23/2 * * * "*/
-	stock: "0 * * * * *"
+	stock: "0 50 9-23/2 * * * "
 };
 
 log.info(`Topic Weather started at ${cron.weather}`);

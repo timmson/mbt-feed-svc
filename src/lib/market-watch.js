b6c83dbd4a336602d6406a15164a559bb1a3dfe2
timmson <timmson666@mail.ru>
2022-12-28 00:59:12 +0300

    Change to MarketWatch

--- src/lib/market-watch.js
+++ src/lib/market-watch.js
@@ -1,4 +1,8 @@
 const cheerio = require("cheerio")
+const fetch = require("node-fetch")
+const log = require("log4js").getLogger("market-watch")
+log.level = "info"
+
 const baseUrl = "https://www.marketwatch.com/investing/index/"
 
 class MarketWatch {
@@ -6,10 +10,13 @@ class MarketWatch {
 	async getStockPrice(ticket) {
 		try {
 			const url = `${baseUrl + ticket}`
+			log.info(`Fetching ${url}`)
 			const resp = await fetch(url)
 			const body = await resp.text()
 			const $ = cheerio.load(body)
-			return $("meta[name=\"price\"]").attr("content")
+			const value = $("meta[name=\"price\"]").attr("content").replace(",","")
+			log.info(`...OK[${value}}`)
+			return value
 		} catch (e) {
 			console.log(e)
 		}
